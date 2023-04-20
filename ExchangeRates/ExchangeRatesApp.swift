e1226a3e936a81080c2a66a006432f766f6492e6
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-04-20 11:08:13 -0400

    Initial Commit

---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Oliver Finlayson on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
